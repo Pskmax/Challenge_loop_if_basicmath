46ee3bc56f4ba42bf14d888e6ebff5b3e7678bd3
pskmax <pskma@LAPTOP-810SC5FO>
2020-08-10 21:55:43 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a, b, i, j, n = 0;
+	float average = 0, sum = 0, sumSD = 0, SD, c, z;
+	scanf_s("%d %d", &a, &b);
+	if (a < b)
+	{
+		for (i = a; i <= b; i++)
+		{
+			printf("%d ", i);
+			sum += i;
+			n++;
+		}
+		average = sum / n;
+		for (j = a; j <= b; j++)
+		{
+			c = j - average;
+			c = c * c;
+			sumSD += c;
+		}
+		z = sumSD / n;
+		SD = sqrt(z);
+	}
+	if (a > b)
+	{
+		for (i = a; i >= b; i--)
+		{
+			printf("%d ", i);
+			sum += i;
+			n++;
+		}
+		average = sum / n;
+		for (j = b; j <= a; j++)
+		{
+			c = j - average;
+			c = c * c;
+			sumSD += c;
+		}
+		z = sumSD / n;
+		SD = sqrt(z);
+	}
+	printf("\nAverage = %.2f", average);
+	printf("\nSD = %.2f", SD);
+	return 0;
+}
